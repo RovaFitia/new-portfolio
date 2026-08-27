4a5a22c7469e584d6686784d2412e73bf5630862
Rova Fitia <rovafiti@gmail.com>
2026-08-27 22:16:20 +0300

    init: initial of New Codeur Portfolio

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+function App() {
+  return (
+    <div>
+          <h1 className="text-7xl font-solar-md">The retention engine</h1>
+          <h2 className="text-5xl font-pixel">This is a heading</h2>
+          <p className="font-haffer-r">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Reiciendis quis non, ab impedit sint eum, aliquid, fugiat sunt necessitatibus vitae voluptates corporis culpa fugit. Nesciunt alias ratione libero soluta in sint, fugit architecto officia expedita deserunt fugiat quas debitis porro, distinctio quae ducimus dolorem natus! Ducimus eos similique veniam veritatis incidunt, qui delectus quas natus labore sit quibusdam quia officiis consequuntur explicabo nemo? Qui, ut veniam, facere iure id distinctio animi repellat, ab harum consequatur mollitia. Officiis consequatur suscipit quaerat eligendi sequi autem nihil qui animi soluta pariatur, ipsum facilis explicabo quisquam aliquam porro reiciendis error debitis non enim illum.</p>
+          <p className="font-haffer-md">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Reiciendis quis non, ab impedit sint eum, aliquid, fugiat sunt necessitatibus vitae voluptates corporis culpa fugit. Nesciunt alias ratione libero soluta in sint, fugit architecto officia expedita deserunt fugiat quas debitis porro, distinctio quae ducimus dolorem natus! Ducimus eos similique veniam veritatis incidunt, qui delectus quas natus labore sit quibusdam quia officiis consequuntur explicabo nemo? Qui, ut veniam, facere iure id distinctio animi repellat, ab harum consequatur mollitia. Officiis consequatur suscipit quaerat eligendi sequi autem nihil qui animi soluta pariatur, ipsum facilis explicabo quisquam aliquam porro reiciendis error debitis non enim illum.</p>
+           <p className="font-haffer-sb">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Reiciendis quis non, ab impedit sint eum, aliquid, fugiat sunt necessitatibus vitae voluptates corporis culpa fugit. Nesciunt alias ratione libero soluta in sint, fugit architecto officia expedita deserunt fugiat quas debitis porro, distinctio quae ducimus dolorem natus! Ducimus eos similique veniam veritatis incidunt, qui delectus quas natus labore sit quibusdam quia officiis consequuntur explicabo nemo? Qui, ut veniam, facere iure id distinctio animi repellat, ab harum consequatur mollitia. Officiis consequatur suscipit quaerat eligendi sequi autem nihil qui animi soluta pariatur, ipsum facilis explicabo quisquam aliquam porro reiciendis error debitis non enim illum.</p>
+    </div>
+  )
+}
+
+export default App
