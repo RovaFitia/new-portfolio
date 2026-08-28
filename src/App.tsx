76af2b931e8f2dfaa77bd95538ae91649f7e7a0c
Rova Fitia <rovafiti@gmail.com>
2026-08-28 08:28:59 +0300

    add: create design systme

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,22 @@
+import { Container } from "@/components/layout/Container"
+
 function App() {
   return (
-    <div>
-          <h1 className="text-7xl font-solar-md">The retention engine</h1>
-          <h2 className="text-5xl font-pixel">This is a heading</h2>
-          <p className="font-haffer-r">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Reiciendis quis non, ab impedit sint eum, aliquid, fugiat sunt necessitatibus vitae voluptates corporis culpa fugit. Nesciunt alias ratione libero soluta in sint, fugit architecto officia expedita deserunt fugiat quas debitis porro, distinctio quae ducimus dolorem natus! Ducimus eos similique veniam veritatis incidunt, qui delectus quas natus labore sit quibusdam quia officiis consequuntur explicabo nemo? Qui, ut veniam, facere iure id distinctio animi repellat, ab harum consequatur mollitia. Officiis consequatur suscipit quaerat eligendi sequi autem nihil qui animi soluta pariatur, ipsum facilis explicabo quisquam aliquam porro reiciendis error debitis non enim illum.</p>
-          <p className="font-haffer-md">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Reiciendis quis non, ab impedit sint eum, aliquid, fugiat sunt necessitatibus vitae voluptates corporis culpa fugit. Nesciunt alias ratione libero soluta in sint, fugit architecto officia expedita deserunt fugiat quas debitis porro, distinctio quae ducimus dolorem natus! Ducimus eos similique veniam veritatis incidunt, qui delectus quas natus labore sit quibusdam quia officiis consequuntur explicabo nemo? Qui, ut veniam, facere iure id distinctio animi repellat, ab harum consequatur mollitia. Officiis consequatur suscipit quaerat eligendi sequi autem nihil qui animi soluta pariatur, ipsum facilis explicabo quisquam aliquam porro reiciendis error debitis non enim illum.</p>
-           <p className="font-haffer-sb">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Reiciendis quis non, ab impedit sint eum, aliquid, fugiat sunt necessitatibus vitae voluptates corporis culpa fugit. Nesciunt alias ratione libero soluta in sint, fugit architecto officia expedita deserunt fugiat quas debitis porro, distinctio quae ducimus dolorem natus! Ducimus eos similique veniam veritatis incidunt, qui delectus quas natus labore sit quibusdam quia officiis consequuntur explicabo nemo? Qui, ut veniam, facere iure id distinctio animi repellat, ab harum consequatur mollitia. Officiis consequatur suscipit quaerat eligendi sequi autem nihil qui animi soluta pariatur, ipsum facilis explicabo quisquam aliquam porro reiciendis error debitis non enim illum.</p>
+      <div>
+          <Container className="mt-5">
+            <h1 className="text-7xl font-solar-md">The retention engine</h1>
+            <h2 className="text-5xl font-pixel">This is a heading</h2>
+            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Reiciendis quis non, ab impedit sint eum, aliquid, fugiat sunt necessitatibus vitae voluptates corporis culpa fugit. Nesciunt alias ratione libero soluta in sint, fugit architecto officia expedita deserunt fugiat quas debitis porro, distinctio quae ducimus dolorem natus! Ducimus eos similique veniam veritatis incidunt, qui delectus quas natus labore sit quibusdam quia officiis consequuntur explicabo nemo? Qui, ut veniam, facere iure id distinctio animi repellat, ab harum consequatur mollitia. Officiis consequatur suscipit quaerat eligendi sequi autem nihil qui animi soluta pariatur, ipsum facilis explicabo quisquam aliquam porro reiciendis error debitis non enim illum.</p>
+            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Reiciendis quis non, ab impedit sint eum, aliquid, fugiat sunt necessitatibus vitae voluptates corporis culpa fugit. Nesciunt alias ratione libero soluta in sint, fugit architecto officia expedita deserunt fugiat quas debitis porro, distinctio quae ducimus dolorem natus! Ducimus eos similique veniam veritatis incidunt, qui delectus quas natus labore sit quibusdam quia officiis consequuntur explicabo nemo? Qui, ut veniam, facere iure id distinctio animi repellat, ab harum consequatur mollitia. Officiis consequatur suscipit quaerat eligendi sequi autem nihil qui animi soluta pariatur, ipsum facilis explicabo quisquam aliquam porro reiciendis error debitis non enim illum.</p>
+              <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Reiciendis quis non, ab impedit sint eum, aliquid, fugiat sunt necessitatibus vitae voluptates corporis culpa fugit. Nesciunt alias ratione libero soluta in sint, fugit architecto officia expedita deserunt fugiat quas debitis porro, distinctio quae ducimus dolorem natus! Ducimus eos similique veniam veritatis incidunt, qui delectus quas natus labore sit quibusdam quia officiis consequuntur explicabo nemo? Qui, ut veniam, facere iure id distinctio animi repellat, ab harum consequatur mollitia. Officiis consequatur suscipit quaerat eligendi sequi autem nihil qui animi soluta pariatur, ipsum facilis explicabo quisquam aliquam porro reiciendis error debitis non enim illum.</p>
+
+              <div className="flex flex-col items-start mt-5">
+                <button className="px-5 py-2 border border-primary bg-primary text-white rounded-3xl">Click me</button>
+                <button>Contact me</button>
+
+              </div>
+
+          </Container>
     </div>
   )
 }
